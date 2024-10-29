d2599a7053b4a099db61f08de76bf6e0142a8e41
TimeMachine1994 <Rockodo@gmail.com>
2024-10-29 15:16:03 +0000

    Updated correct build for cloudflare

--- TributestreamWeb/svelte.config.js
+++ TributestreamWeb/svelte.config.js
@@ -16,7 +16,7 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
-		outDir: 'build',
+		outDir: '.svelte-kit/cloudflare',
 	}
 };
 export default config;
