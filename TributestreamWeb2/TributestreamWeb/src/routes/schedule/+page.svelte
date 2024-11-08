2a8a8c7649f779c083f8763dd5305a5ed96106ec
TimeMachine1994 <Rockodo@gmail.com>
2024-11-08 20:14:01 +0000

    fdafadsf

--- TributestreamWeb2/TributestreamWeb/src/routes/schedule/+page.svelte
+++ TributestreamWeb2/TributestreamWeb/src/routes/schedule/+page.svelte
@@ -13,7 +13,7 @@
       const password = generateRandomPassword();
       
       try {
-        const response = await fetch('https://tributestream.com/wp-json/my-plugin/v1/register', {
+        const response = await fetch('https://wp.tributestream.com/wp-json/my-plugin/v1/register', {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify({
