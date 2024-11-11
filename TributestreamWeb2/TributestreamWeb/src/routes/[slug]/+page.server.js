29d0101d376a478217d44084304c780f7853556f
TimeMachine1994 <Rockodo@gmail.com>
2024-11-11 20:43:34 +0000

    asdf

--- TributestreamWeb2/TributestreamWeb/src/routes/[slug]/+page.server.js
+++ TributestreamWeb2/TributestreamWeb/src/routes/[slug]/+page.server.js
@@ -5,11 +5,7 @@ export async function load({ params, fetch }) {
         // Fetch tribute data from the REST API
         const response = await fetch(`https://wp.tributestream.com/wp-json/tributestream/v1/tribute/${slug}`);
 
-        // Check if the response is OK (status code 200-299)
-        if (!response.ok) {
-            return { status: response.status, error: new Error(`Error fetching tribute: ${response.statusText}`) };
-        }
-
+ 
         // Parse the JSON data from the response
         const tributeData = await response.json();
 
