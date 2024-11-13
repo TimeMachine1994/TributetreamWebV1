e4729d061437d475128622675cd2834465335298
CryptoquakeTV <rockodo@gmail.com>
2024-11-13 02:08:50 +0000

    asdf

--- TributestreamWeb2/TributestreamWeb/src/routes/celebration-of-life-for-[slug]/+page.js
+++ TributestreamWeb2/TributestreamWeb/src/routes/celebration-of-life-for-[slug]/+page.js
@@ -1,6 +1,13 @@
 export async function load({ data }) {
+    if (data?.redirect) {
+        // This should ideally not be necessary since redirect is server-handled.
+        // However, adding this as a safeguard on client-side navigation.
+        window.location.href = data.redirect;
+        return {};
+    }
+    
     return {
         ...data,
-        // Add any client-side transformations here
+        // You can add client-side transformations here if needed.
     };
 }
