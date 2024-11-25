70fd1dd6715af9f6f9c7795586f99dff0821651c
CryptoquakeTV <rockodo@gmail.com>
2024-11-24 21:34:18 -0500

    Seeing in login works

---
+++ TributestreamWeb2/TributestreamWeb/src/routes/+error.svelte
@@ -0,0 +1,3 @@
+Tributestream is currentling undergoing scheduled matenance. We are working as hard as we can to bring you back online.
+
+If you still see this message after 24 hours, please contact us at support@tributestream.com and we can provide a direct link to your loved one's Tributestream.
