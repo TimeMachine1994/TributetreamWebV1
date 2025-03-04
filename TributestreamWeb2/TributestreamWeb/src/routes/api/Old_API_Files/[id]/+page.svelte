55eaf6ef3bf34375ce8e9a37d80c6794a5c9cbec
CryptoquakeTV <rockodo@gmail.com>
2025-03-04 16:04:40 +0000

    Cleaning up

--- TributestreamWeb2/TributestreamWeb/src/routes/api/Old_API_Files/[id]/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-    let { tribute } = $props();
-</script>
-
-<main>
-    <h1>{tribute.loved_one_name}</h1>
-    <p>Created by: {tribute.user_id}</p>
-    <p>{tribute.custom_html}</p>
-</main>
