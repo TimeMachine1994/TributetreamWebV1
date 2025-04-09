13eac75a93a3eb82d6af89414e3192c9676336b3
TimeMachine1994 <rockodo>
2025-04-09 17:45:03 -0400

    Good?

--- TributestreamDev-Version03/src/routes/fd-form/+page.svelte
+++ TributestreamDev-Version03/src/routes/fd-form/+page.svelte
@@ -566,14 +566,7 @@
 
     <!-- Action Buttons -->
     <div class="flex justify-between">
-      <!-- Test Data Button -->
-      <button
-        type="button"
-        class="bg-gray-500 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded"
-        on:click={fillTestData}
-      >
-        Fill Test Data
-      </button>
+   
       
       <!-- Submit Button -->
       <button
