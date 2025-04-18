726b66e1fedcc967d489d08cdca8194291c840c0
CryptoquakeTV <rockodo@gmail.com>
2025-04-18 19:33:20 +0000

    probably changing to strapi

---
+++ TributestreamDev-Version05/TributestreamDB/src/api/tribute/controllers/tribute.ts
@@ -0,0 +1,7 @@
+/**
+ * tribute controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::tribute.tribute');
---
+++ TributestreamDev-Version05/TributestreamDB/src/api/tribute/routes/tribute.ts
@@ -0,0 +1,7 @@
+/**
+ * tribute router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::tribute.tribute');
